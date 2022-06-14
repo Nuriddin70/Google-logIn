bcb9ec1f883d75e77369e08b40c79d8801472aef
Nuriddin70 <nuriddin3050@gmail.com>
2022-06-14 22:20:34 +0900

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Box, Container } from "@mui/system";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component='main' maxWidth='xs'>
+      <Box
+        sx={{
+          margionTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <h2>
+          Sing in
+        </h2>
+        <TextField
+          margin="normal"
+          label="Email Address"
+          required
+          fullWidth
+          name="email"
+          autoComplete="emails"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          label="Password"
+          type="password"
+          required
+          fullWidth
+          name="email"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me!"
+        />
+
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb:2}}>
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link>Forgot password?</Link>
+          </Grid>
+          <Grid item>
+            <Link> Sign Up</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
